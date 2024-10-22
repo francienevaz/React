6d48f4a610e7da18e974e30750486c2b4b46fa53
francienevaz <fran.vfraga@gmail.com>
2024-10-22 16:52:17 -0300

    working with useEffect

--- Req_HTTP/http_react/src/App.jsx
+++ Req_HTTP/http_react/src/App.jsx
@@ -1,11 +1,94 @@
 import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
+  const url = "http://localhost:3000/products"
+
+  const [ products, setProducts ] = useState([]);
+
+  const [ name, setName ] = useState("");
+  const [ price, setPrice ] = useState("");
   
+  // resgatando dados
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+
+        const data = await res.json()
+
+        setProducts(data)
+
+      } catch (err) {
+        console.error("Erro ao buscar produtos", err);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  // adicionando produtos - POST
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // quando setamos o "name" no input, nao precisamos especificar aqui, exemplo: 
+    // const product = {
+    // name: {name}, 
+    // price: {price}
+    // }  -- o Javascript ja identifica 
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    // Carregamento dinamico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("");
+    setPrice("");
+  };
+
+
   return (
-    <div className='App'>
-      <h1>Lista de Produtos</h1>
-    </div>
+    <>
+      <div className='App'>
+        <h1>Lista de Produtos</h1>
+        <ul>
+          {/* usamos o () ao inves do {} na função para retorna o objeto */}
+          {products.map((item) => (
+            <li key={item.id}>
+              Produto: {item.name} - R${item.price} </li>
+          ))}
+        </ul>
+
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span>Nome:</span>
+              <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              <span>Preço:</span>
+              <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <input type="submit" value="Criar" />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
